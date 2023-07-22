421dd6e363bb3696155e8be850d332a94e7519b8
Farahat612 <67427124+Farahat612@users.noreply.github.com>
2023-07-22 23:08:11 +0000

    Added Dynamic Database-driven pages for jobs and configured shared layout, styles and navigation using bootstrap.

--- database.py
+++ database.py
@@ -17,3 +17,16 @@ def load_jobs_from_db():
       row_dict = row._asdict()  #converting to dict
       jobs_as_dicts.append(row_dict)
     return jobs_as_dicts
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val "),
+                          {"val": id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      job = rows[0]._asdict()
+      return job
+
